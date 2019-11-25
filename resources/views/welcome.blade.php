94331005ab86ab478c170934f1908d0f8e39ef11
kainzs <profit7326@gmail.com>
2019-11-25 11:57:15 +0900

    코드확인

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,6 +94,7 @@
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                     <a href="{{ route('questions.index') }}">question</a>
+                    
                 </div>
             </div>
         </div>
