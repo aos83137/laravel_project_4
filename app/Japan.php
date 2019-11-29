ca23b4eea68ee66f7155abc8a834ccd60014531a
kainzs <profit7326@gmail.com>
2019-11-29 17:06:49 +0900

    현지학기제페이지 ajax이용하여 C,R까지 구현완료

---
+++ app/Japan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Japan extends Model
+{
+    protected $fillable = ['title','content','destination','week','image'];
+}
--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Yajra\Datatables\DatatablesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -174,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+
         Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
@@ -227,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
