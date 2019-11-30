8b9db32eaa87c37b196e121f18d024ae4fcaa95f
YongSeok <qazwsx1455@gmail.com>
2019-11-30 23:14:59 +0900

    q&a list 수정

--- resources/views/view/comment.blade.php
+++ resources/views/view/comment.blade.php
@@ -12,10 +12,8 @@
                         },
 
                         }).then(function(data){
-                            // alert(data.success);
-                            var $div = $('<div class="commentsContents'+10+'"><hr>id : {{ $user->name }}<br>commnet : '+data.comment+' <br> <button name="delete" class="btn btn-danger button__delete" data-id="10" data-cnt="" >삭제</button>   <hr></div>');
+                            var $div = $('<div class="commentsContents'+10+'"><hr>id : {{ $user->name }}<br>comment : '+data.comment+' <br> <button name="delete" class="btn btn-danger button__delete btn-sm" data-id="10" data-cnt="" >삭제</button>   <hr></div>');
                             $('#comments').append($div);
-                            
                         });
                     }else{
                         // error messag
@@ -24,21 +22,16 @@
         });
 
 </script>
-{{-- <form action="{{ route('comments.store',$id) }}" method="POST"> --}}
 @csrf
 <h3>댓글 달기</h3>
 <input type="text" name="comment" class="form-control" id="comment" value="">
 
 @auth
-    <button class="btn btn-danger button__add">등록</button>
-    {{-- <button name="delete" class="btn btn-danger button__delete" data-id="{{ $comment->id }}" >삭제</button> --}}
-
-    {{-- <input type="submit" class="btn btn-danger" value="등록"/>   --}}
+    <button class="btn btn-outline-primary button__add ">등록</button>
 @endauth
 @guest
     <p>로그인 해주세요</p>
 @endguest
 
 {!! $errors->first('comment', '<span class="form-error">:message</span>') !!}
-{{-- </form> --}}
 
