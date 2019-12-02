aff69ec0a0e9d5e233a57ea910086c97e25eca1a
Lee <r7042@naver.com>
2019-12-02 23:40:12 +0900

    main_css

--- resources/views/japans/show.blade.php
+++ resources/views/japans/show.blade.php
@@ -3,19 +3,59 @@
 <head>
     <title>Datatables Server Side Processing in Laravel</title>
 </head>
+<style>
+body, html{
+  background-color:skyblue;
+  background: repeating-linear-gradient(45deg, white 50%,skyblue 30%,);
+  font-family: 'Nunito', sans-serif;
+}
+@keyframes slidein {
+                0%, 100%{transform:translate(0,0)}
+                50%{transform:translate(0,-20px)}
+            }
+            @-webkit-keyframes bounce{
+              0%, 100%{transform:translate(0,0)}
+              50%{transform:translate(0,-20px)}
+            }
+          }
+.jumbotron{
+  text-align:center;
+  /* border: 1px solid brown; */
+  animation-name: slidein;
+  animation-duration:2s;
+  animation-iteration-count:infinite;
+  animation-direction:normal;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: both;
+  animation-delay: 1s;
+}
+.container{
+  text-align:center;
+}
+.text{
+  text-align:left;
+  /* margin-left:30px; */
+  margin-top:70px;
+}
+</style>
 <body>
-
 <div class="container">
     <br />
-    <h3 align="center">It is for u</h3>
+    <h2>it is for u</h2>
     <br />
-    <div class="jumbotron text-center">
+    <div class="jumbotron">
       <img src="{{URL::to('/')}}/images/{{$data->image}}" alt="">
+    </div>
+    <div class="text">
       <h3>week - {{$data->week}}</h3>
+      <hr>
       <h3>destination- {{$data->destination}}</h3>
+      <hr>
       <h3>title- {{$data->title}}</h3>
+      <hr>
       <h3>content- {{$data->content}}</h3>
-
-
+      <hr>
     </div>
 </div>
+</body>
+</html>
