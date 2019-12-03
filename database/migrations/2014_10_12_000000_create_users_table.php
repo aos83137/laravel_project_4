583546ac3596b53e3522dc8493646b680ec24925
rlaehwns <rlaehwns9698@naver.com>
2019-12-03 12:26:08 +0900

    디비 변경

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('verifyToken');
-            $table->boolean('status');
+            $table->string('verifyToken')->nullable();
+            $table->boolean('status')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
